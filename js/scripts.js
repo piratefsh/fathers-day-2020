2ccbf631ee1cd072ec439ba5cb2657b4e5ba551d
Sher Minn Chong <sherminn.chong@gmail.com>
2020-06-13 22:04:40 -0400

    Make scattered polaroid

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
 // window.alert('hi ')
+
+const photos = document.querySelectorAll('.photos li');
+
+const random = (start, end) => {
+  return start + Math.random() * (end-start);
+}
+
+photos.forEach((photo) => {
+  photo.style.setProperty('--rot', `${random(-15, 15)}deg`);
+  photo.style.setProperty('--posx', `${random(-40, 40)}px`);
+  photo.style.setProperty('--posy', `${random(-40, 40)}px`);
+})
