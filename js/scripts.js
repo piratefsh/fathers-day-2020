00703b1d485e47974a154a03e083adf739d9bd84
Sher Minn Chong <sherminn.chong@gmail.com>
2020-06-20 09:46:40 -0400

    Add slideshow

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,20 @@ photos.forEach((photo) => {
   photo.style.setProperty('--posy', `${random(-50, 50)}px`);
   photo.style.setProperty('--zindex', `${random(1, 5)}px`);
 })
+
+const slideshows = document.querySelectorAll('.slideshow');
+
+slideshows.forEach((ss) => {
+  let currentIndex = 0;
+  const slides = ss.querySelectorAll('li');
+  slides[0].classList.add('show');
+  setInterval(() => {
+    slides.forEach((s, i) => {
+      s.classList.remove('show');
+      if(currentIndex % i === 0){
+        s.classList.add('show');
+      }
+      currentIndex++;
+    })
+  }, 5 * 1000)
+})
